dd9a5d1123be13c5a661cc0ab4f9e123a01a4d08
Chris Fellows <chris.fellows@OKTiK.net>
2025-01-18 13:11:53 +0000

    chore: Code cleanup

---
+++ CFWebServerCommon/Interfaces/IWebServer.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CFWebServer.Interfaces
+{
+    /// <summary>
+    /// Interface to web server
+    /// </summary>
+    public interface IWebServer
+    {
+        /// <summary>
+        /// Adds site
+        /// </summary>
+        /// <param name="site"></param>
+        void Add(ISite site);
+
+        /// <summary>
+        /// All sites
+        /// </summary>
+        List<ISite> Sites { get; }
+    }
+}
---
+++ CFWebServerCommon/WebServer.cs
@@ -0,0 +1,16 @@
+﻿using CFWebServer.Interfaces;
+
+namespace CFWebServer
+{
+    public class WebServer : IWebServer
+    {
+        private List<ISite> _sites = new List<ISite>();
+
+        public void Add(ISite site)
+        {
+            _sites.Add(site);
+        }
+
+        public List<ISite> Sites => _sites;
+    }
+}
